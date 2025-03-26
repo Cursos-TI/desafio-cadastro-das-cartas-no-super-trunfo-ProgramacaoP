12bb0250f06577a085567c4a594e7683d2e4c8d9
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-26 02:08:46 +0000

    comentario sobre definição das variaveis, segudo das variaveis definidas

--- Super_Trunfo/Super_trunfo_CartaB01.c
+++ Super_Trunfo/Super_trunfo_CartaB01.c
@@ -4,4 +4,13 @@ int main() {
     printf("Cadastro da Carta B01\n");
     // Acima printf para definir qual carta está sendo cadastrada
 
+    // Abaixo definição de variáveis para cada atributo da carta
+    char Estado[1] = "B";
+    char CodigodaCarta[03] = "B01";
+    char Cidade[20] = "Fortaleza";
+    int Populacao = 2428678;
+    float Area = 312.3;
+    float PIB = 74.4;
+    int PontosTuristicos = 17;
+
 }
