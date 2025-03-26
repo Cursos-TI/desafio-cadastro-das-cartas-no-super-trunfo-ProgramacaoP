1dcb6a37e04010057c0d1bef96233389c58014ad
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-26 01:48:41 +0000

    definindo e inicializando variveis variaveis inserindo os dados da respectiva carta

--- Super_Trunfo/Super_trunfo.c
+++ Super_Trunfo/Super_trunfo.c
@@ -4,5 +4,12 @@ int main() {
     printf("Cadastro da Cartas A01\n");
     // Acima printf para definir qual carta está sendo cadastrada
     // Abaixo definição de variáveis para cada atributo da carta
+    char Estado[1] = "A";
+    char CodigodaCarta[03] = "A01";
+    char Cidade[20] = "Salvador";
+    int Populacao = 2418000;
+    float Area = 693.8;
+    float PIB = 21.8;
+    int PontosTuristicos = 20;
 
 }
