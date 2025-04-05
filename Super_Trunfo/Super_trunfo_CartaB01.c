66e362c2e8aa065fbbfeecbd3d7314543a51ba14
ProgramacaoP <paulalisboarp@gmail.com>
2025-04-05 13:11:25 +0000

    INSERINDO FUNCIONALIDADE DE DIVISÃO E EXECUTANDO EXIBIÇÃO DE DADOS - CARTA B01

--- Super_Trunfo/Super_trunfo_CartaB01.c
+++ Super_Trunfo/Super_trunfo_CartaB01.c
@@ -40,6 +40,7 @@ int main() {
     printf("Area: %.2f\n", Area);
     printf("PIB: %.2f\n", PIB);
     printf("Numero de Pontos Turisticos: %d\n", PontosTuristicos);
+    //inserindo novas funcionalidade de divisão e exibição
 
     return 0;
     
