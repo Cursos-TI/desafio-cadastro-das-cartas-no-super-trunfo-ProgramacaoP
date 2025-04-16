9d4da70ff5040666c2584a401bf59516624678af
ProgramacaoP <paulalisboarp@gmail.com>
2025-04-16 23:39:50 +0000

    ALTERAÇOES, INSERIDO TODOS OS COMPARATIVOS

--- Super_Trunfo/Super_Trunfo.c
+++ Super_Trunfo/Super_Trunfo.c
@@ -116,28 +116,65 @@ int main() {
     printf("Super Poder:%.2f\n", CartaII.SuperPoder);
 
     // Abaixo comparação entre as cartas - Nivel Aventureiro
-    printf("\n\nComparação entre as cartas\n");
-    printf("Densidade Populacional da Carta I: %.2f\n", CartaI.densidadePopulacional);
+    printf("\n\nComparação entre as cartas\n\n");
+    printf("População da Carta I: %d\n", CartaI.populacao);
+    printf("População da Carta II: %d\n", CartaII.populacao);
+    if (CartaI.populacao > CartaII.populacao) {
+        printf("A Carta I venceu! Tem maior população.\n");
+    } else if (CartaI.populacao < CartaII.populacao) {
+        printf("A Carta II venceu! Tem maior população.\n");
+    } else {
+        printf("As cartas venceu! Têm a mesma população.\n");
+    }
+    printf("\nÁrea da Carta I: %.2f\n", CartaI.area);
+    printf("Área da Carta II: %.2f\n", CartaII.area);
+    if (CartaI.area > CartaII.area) {
+        printf("A Carta I venceu! Tem maior área.\n");
+    } else if (CartaI.area < CartaII.area) {
+        printf("A Carta II venceu! Tem maior área.\n");
+    } else {
+        printf("As cartas têm a mesma área.\n");
+    }
+    printf("\nPIB da Carta I: %.2f\n", CartaI.pib);
+    printf("PIB da Carta II: %.2f\n", CartaII.pib);
+    if (CartaI.pib > CartaII.pib) {
+        printf("A Carta I venceu! Tem maior PIB.\n");
+    } else if (CartaI.pib < CartaII.pib) {
+        printf("A Carta II venceu! Tem maior PIB.\n");
+    } else {
+        printf("As cartas têm o mesmo PIB.\n");
+    }
+    printf("\nNúmero de Pontos Turísticos da Carta I: %d\n", CartaI.pontosTuristicos);
+    printf("Número de Pontos Turísticos da Carta II: %d\n", CartaII.pontosTuristicos);
+    if (CartaI.pontosTuristicos > CartaII.pontosTuristicos) {
+        printf("A Carta I venceu! Tem mais pontos turísticos.\n");
+    } else if (CartaI.pontosTuristicos < CartaII.pontosTuristicos) {
+        printf("A Carta II venceu! Tem mais pontos turísticos.\n");
+    } else {
+        printf("As cartas têm o mesmo número de pontos turísticos.\n");
+    }
+    printf("\nDensidade Populacional da Carta I: %.2f\n", CartaI.densidadePopulacional);
     printf("Densidade Populacional da Carta II: %.2f\n", CartaII.densidadePopulacional); 
+    
      
     if (CartaI.densidadePopulacional > CartaII.densidadePopulacional) {
-        printf("A Carta I tem maior densidade populacional.\n");
+        printf("A Carta I venceu! Tem maior densidade populacional.\n");
     } else if (CartaI.densidadePopulacional < CartaII.densidadePopulacional) {
-        printf("A Carta II tem maior densidade populacional.\n");
+        printf("A Carta II venceu! Tem maior densidade populacional.\n");
     } else {
-        printf("As cartas têm a mesma densidade populacional.\n");
+        printf("As cartas venceu! Têm a mesma densidade populacional.\n");
     }
-    printf("PIB Per Capita da Carta I: %.2f\n", CartaI.pibPerCapita);
+    printf("\nPIB Per Capita da Carta I: %.2f\n", CartaI.pibPerCapita);
     printf("PIB Per Capita da Carta II: %.2f\n", CartaII.pibPerCapita);
     if (CartaI.pibPerCapita > CartaII.pibPerCapita) {
-        printf("A Carta I tem maior PIB per capita.\n");
+        printf("A Carta I venceu! Tem maior PIB per capita.\n");
     } else if (CartaI.pibPerCapita < CartaII.pibPerCapita) {
-        printf("A Carta II tem maior PIB per capita.\n");
+        printf("A Carta II venceu! Tem maior PIB per capita.\n");
     } else {
         printf("As cartas têm o mesmo PIB per capita.\n");
     }
      // Comparação de Super Poder
-     printf("Super Poder da Carta I: %.2f\n", CartaI.SuperPoder);
+     printf("\nSuper Poder da Carta I: %.2f\n", CartaI.SuperPoder);
      printf("Super Poder da Carta II: %.2f\n", CartaII.SuperPoder);
      if (CartaI.SuperPoder > CartaII.SuperPoder) {
          printf("\nA Carta I venceu! Tem o maior Super Poder.\n");
