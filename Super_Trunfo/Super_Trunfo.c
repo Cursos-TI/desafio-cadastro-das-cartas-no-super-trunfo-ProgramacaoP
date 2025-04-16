7e67b504f08272f2a4a56b8a911d4644e1502652
ProgramacaoP <paulalisboarp@gmail.com>
2025-04-16 23:06:11 +0000

    ALTERAÇÕES E CORREÇÕES NO LINK PARA RECEBER FUNCIONALIDADE DO NOVO DESAFIO

--- Super_Trunfo/Super_Trunfo.c
+++ Super_Trunfo/Super_Trunfo.c
@@ -5,120 +5,147 @@ int main() {
     // Acima printf para informando quis cartas estão sendo cadastradas - Nivel Novato
 
     // Abaixo definição de variáveis para cada atributo da carta - Nivel Novato
-    //DADOS DA CARTA I
-    char EstadoI[03] = "A";
-    char CodigodaCartaI[05] = "A01";
-    char CidadeI[12] = "Salvador";
-    int PopulacaoI = 2418000;
-    float AreaI = 693.8;
-    float PibI = 21.8;
-    int PontosTuristicosI = 20;
+    // Definição da estrutura para as cartas
+    typedef struct {
+        char estado[3];
+        char codigo[5];
+        char cidade[12];
+        int populacao;
+        float area;
+        float pib;
+        int pontosTuristicos;
+        float densidadePopulacional;
+        float pibPerCapita;
+        float SuperPoder;
+    } Carta;
+    Carta CartaI, CartaII;
+    // Inicialização das cartas
+    CartaI.estado, "A";
+    CartaI.codigo, "A01";
+    CartaI.cidade, "Salvador";
+    CartaI.populacao = 2418000;
+    CartaI.area = 693.8;
+    CartaI.pib = 21.8;
+    CartaI.pontosTuristicos = 20;
+    CartaI.densidadePopulacional = CartaI.populacao / CartaI.area;
+    CartaI.pibPerCapita =  CartaI.populacao / CartaI.pib;
+    CartaI.SuperPoder = (CartaI.populacao + CartaI.area + CartaI.pib + CartaI.pontosTuristicos + CartaI.densidadePopulacional + CartaI.pibPerCapita) / 2;
 
-    //Inserindo novas variáveis - Nivel Aventureiro
-    float DensidadePopulacionalI = PopulacaoI / AreaI;
-    float PibPerCaptaI = PopulacaoI / PibI;
+    CartaII.estado[0] = 'B'; CartaII.estado[1] = '\0';
+    CartaII.codigo, "B01";
+    CartaII.cidade, "Fortaleza";
+    CartaII.populacao = 2428678;
+    CartaII.area = 312.3;
+    CartaII.pib = 74.4;
+    CartaII.pontosTuristicos = 17;
+    CartaII.densidadePopulacional = CartaII.populacao / CartaII.area;
+    CartaII.pibPerCapita = CartaII.populacao / CartaII.pib;
+    CartaII.SuperPoder = (CartaII.populacao + CartaII.area + CartaII.pib + CartaII.pontosTuristicos + CartaII.densidadePopulacional + CartaII.pibPerCapita) / 2;
 
+    // Cálculo de densidade populacional e PIB per capita
+    CartaI.densidadePopulacional =   CartaI.populacao / CartaI.area;
+    CartaI.pibPerCapita = CartaI.populacao / CartaI.pib;
     
-    // Abaixo definição de variáveis para cada atributo da carta - Nivel Novato
-    //DADOS DA CARTA II
-    char EstadoII[03] = "B";
-    char CodigodaCartaII[05] = "B01";
-    char CidadeII[12] = "Fortaleza";
-    int PopulacaoII = 2428678;
-    float AreaII = 312.3;
-    float PibII = 74.4;
-    int PontosTuristicosII = 17;
-
-    //Inserindo novas variáveis - Nivel Aventureiro
-    float DensidadePopulacionalII = PopulacaoII / AreaII;
-    float PibPerCaptaII = PopulacaoII / PibII;
+    CartaII.densidadePopulacional = CartaII.populacao / CartaII.area;
+    CartaII.pibPerCapita = CartaII.populacao / CartaII.pib;
 
     // Abaixo solicitação de entrada de dados para cada atributo da carta - Nivel Novato
     printf("\nUsuário, insira os dados para a Carta I\n");
     printf("Digite o Estado: ");
-    scanf("%s", EstadoI);
+    scanf("%s", CartaI.estado);
     printf("Digite o Codigo da Carta: ");
-    scanf("%s", CodigodaCartaI);
+    scanf("%s", CartaI.codigo);
     printf("Digite o Nome da cidade: ");
-    scanf("%s", CidadeI);
+    scanf("%s", CartaI.cidade);
     printf("Digite a Populacao: ");
-    scanf("%d", &PopulacaoI);
+    scanf("%d", &CartaI.populacao); 
     printf("Digite a Area: ");
-    scanf("%f", &AreaI);
+    scanf("%f", &CartaI.area);
     printf("Digite o PIB: ");
-    scanf("%f", &PibII);
+    scanf("%f", &CartaI.pib);
     printf("Digite o Numero de Pontos Turisticos:  ");
-    scanf("%d", &PontosTuristicosI);
+    scanf("%d", &CartaI.pontosTuristicos);
+    //inserindo novas funcionalidade de divisão e exibição - Nivel Aventureiro
+    printf("A Densidade populaçional é: %.2f \n", CartaI.densidadePopulacional);
+    printf("O Pib Per Capta é de: %.2f \n", CartaI.pibPerCapita);
+    printf("O Super Poder é de: %.2f \n", CartaI.SuperPoder);
 
     printf("\nUsuário, insira os dados para a Carta II\n");
     printf("Digite o Estado: ");
-    scanf("%s", EstadoII);
+    scanf("%s", CartaII.estado);
     printf("Digite o Codigo da Carta: ");
-    scanf("%s", CodigodaCartaII);
+    scanf("%s", CartaII.codigo);
     printf("Digite o Nome da cidade: ");
-    scanf("%s", CidadeII);
+    scanf("%s", CartaII.cidade);
     printf("Digite a Populacao: ");
-    scanf("%d", &PopulacaoII);
+    scanf("%d", &CartaII.populacao);
     printf("Digite a Area: ");
-    scanf("%f", &AreaII);
+    scanf("%f", &CartaII.area);
     printf("Digite o PIB: ");
-    scanf("%f", &PibII);
+    scanf("%f", &CartaII.pib);
     printf("Digite o Numero de Pontos Turisticos:  ");
-    scanf("%d", &PontosTuristicosII);
+    scanf("%d", &CartaII.pontosTuristicos);
+    //inserindo novas funcionalidade de divisão e exibição - Nivel Aventureiro
+    printf("A Densidade populaçional é: %.2f \n", CartaII.densidadePopulacional);
+    printf("O Pib Per Capta é de: %.2f \n", CartaII.pibPerCapita);
+    printf("O Super Poder é de: %.2f \n", CartaII.SuperPoder);
+
     // Abaixo exibição dos dados das cartas - Nivel Novato
     printf("\n\nDados inseridos pelo usuário para a Carta I\n");
-    printf("Estado:%s\n", EstadoI);
-    printf("Codigo da Carta:%s\n", CodigodaCartaI);
-    printf("Nome da Cidade:%s\n", CidadeI);
-    printf("Populacao: %d\n", PopulacaoI);
-    printf("Area: %.2f\n", AreaI);
-    printf("PIB: %.2f\n", PibI);
-    printf("Numero de Pontos Turisticos: %d\n", PontosTuristicosI);
-
-    //inserindo novas funcionalidade de divisão e exibição - Nivel Aventureiro
-    printf("A Densidade populaçional é: %.2f \n", DensidadePopulacionalI);
-    printf("O Pib Per Capta é de: %.2f \n", PibPerCaptaI);
-
-    printf("\n\nDados inseridos pelo usuário para a Carta II\n");
-    printf("Estado: %s\n", EstadoII);
-    printf("Codigo da Carta: %s\n", CodigodaCartaII);
-    printf("Nome da Cidade: %s\n", CidadeII);
-    printf("Populacao: %d\n", PopulacaoII);
-    printf("Area: %.2f\n", AreaII);
-    printf("PIB: %.2f\n", PibII);
-    printf("Numero de Pontos Turisticos: %d\n", PontosTuristicosII);
-
-    //inserindo novas funcionalidade de divisão e exibição - Nivel Aventureiro
-    printf("A Densidade populaçional é: %.2f \n", DensidadePopulacionalII);
-    printf("O Pib Per Capta é de: %.2f \n", PibPerCaptaII);
-    // Abaixo exibição dos dados das cartas - Nivel Novato
-    printf("\n\nDados inseridos pelo usuário para a Carta I\n");
-    printf("Estado:%s\n", EstadoI);
-    printf("Codigo da Carta:%s\n", CodigodaCartaI);
-    printf("Nome da Cidade:%s\n", CidadeI);
-    printf("Populacao: %d\n", PopulacaoI);
-    printf("Area: %.2f\n", AreaI);
-    printf("PIB: %.2f\n", PibI);
-    printf("Numero de Pontos Turisticos: %d\n", PontosTuristicosI);
-
-    //inserindo novas funcionalidade de divisão e exibição - Nivel Aventureiro
-    printf("A Densidade populaçional é: %.2f \n", DensidadePopulacionalI);
-    printf("O Pib Per Capta é de: %.2f \n", PibPerCaptaI);
+    printf("Estado:%s\n", CartaI.estado);
+    printf("Codigo da Carta:%s\n", CartaI.codigo);
+    printf("Nome da cidade:%s\n", CartaI.cidade);
+    printf("Populacao:%d\n", CartaI.populacao);
+    printf("Area:%.2f\n", CartaI.area);
+    printf("PIB:%.2f\n", CartaI.pib);
+    printf("Numero de Pontos Turisticos:%d\n", CartaI.pontosTuristicos);
+    printf("Densidade Populacional:%.2f\n", CartaI.densidadePopulacional);
+    printf("PIB Per Capita:%.2f\n", CartaI.pibPerCapita);
+    printf("Super Poder:%.2f\n", CartaI.SuperPoder);
 
     printf("\n\nDados inseridos pelo usuário para a Carta II\n");
-    printf("Estado: %s\n", EstadoII);
-    printf("Codigo da Carta: %s\n", CodigodaCartaII);
-    printf("Nome da Cidade: %s\n", CidadeII);
-    printf("Populacao: %d\n", PopulacaoII);
-    printf("Area: %.2f\n", AreaII);
-    printf("PIB: %.2f\n", PibII);
-    printf("Numero de Pontos Turisticos: %d\n", PontosTuristicosII);
+    printf("Estado:%s\n", CartaII.estado);
+    printf("Codigo da Carta:%s\n", CartaII.codigo);
+    printf("Nome da cidade:%s\n", CartaII.cidade);
+    printf("Populacao:%d\n", CartaII.populacao);
+    printf("Area:%.2f\n", CartaII.area);
+    printf("PIB:%.2f\n", CartaII.pib);  
+    printf("Numero de Pontos Turisticos:%d\n", CartaII.pontosTuristicos);
+    printf("Densidade Populacional:%.2f\n", CartaII.densidadePopulacional);
+    printf("PIB Per Capita:%.2f\n", CartaII.pibPerCapita); 
+    printf("Super Poder:%.2f\n", CartaII.SuperPoder);
 
-    //inserindo novas funcionalidade de divisão e exibição - Nivel Aventureiro
-    printf("A Densidade populaçional é: %.2f \n", DensidadePopulacionalII);
-    printf("O Pib Per Capta é de: %.2f \n", PibPerCaptaII);
-
-   return 0;
-
-  
+    // Abaixo comparação entre as cartas - Nivel Aventureiro
+    printf("\n\nComparação entre as cartas\n");
+    printf("Densidade Populacional da Carta I: %.2f\n", CartaI.densidadePopulacional);
+    printf("Densidade Populacional da Carta II: %.2f\n", CartaII.densidadePopulacional); 
+     
+    if (CartaI.densidadePopulacional > CartaII.densidadePopulacional) {
+        printf("A Carta I tem maior densidade populacional.\n");
+    } else if (CartaI.densidadePopulacional < CartaII.densidadePopulacional) {
+        printf("A Carta II tem maior densidade populacional.\n");
+    } else {
+        printf("As cartas têm a mesma densidade populacional.\n");
+    }
+    printf("PIB Per Capita da Carta I: %.2f\n", CartaI.pibPerCapita);
+    printf("PIB Per Capita da Carta II: %.2f\n", CartaII.pibPerCapita);
+    if (CartaI.pibPerCapita > CartaII.pibPerCapita) {
+        printf("A Carta I tem maior PIB per capita.\n");
+    } else if (CartaI.pibPerCapita < CartaII.pibPerCapita) {
+        printf("A Carta II tem maior PIB per capita.\n");
+    } else {
+        printf("As cartas têm o mesmo PIB per capita.\n");
+    }
+     // Comparação de Super Poder
+     printf("Super Poder da Carta I: %.2f\n", CartaI.SuperPoder);
+     printf("Super Poder da Carta II: %.2f\n", CartaII.SuperPoder);
+     if (CartaI.SuperPoder > CartaII.SuperPoder) {
+         printf("\nA Carta I venceu! Tem o maior Super Poder.\n");
+     } else if (CartaI.SuperPoder < CartaII.SuperPoder) {
+         printf("A Carta II venceu! Tem o maior Super Poder.\n");
+     } else {
+         printf("Empate! As cartas têm o mesmo Super Poder.\n");
+     } 
+    
+    return 0;
 }
